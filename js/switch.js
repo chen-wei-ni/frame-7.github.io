679d785e73b69edb748ffe02121c15cea97ba857
chen-wei-ni <bcg2135@gmail.com>
2022-04-01 16:23:37 +0800

    success

---
+++ js/switch.js
@@ -0,0 +1,46 @@
+$('.switch-list').hide()
+$('.language-pack').click(function(e){
+    e.stopPropagation();
+    $('.switch-list').toggle(300, 'linear')
+})
+$("body").click(function(){
+    $('.switch-list').hide()
+})
+$('.switch-list').click(function(e){
+    e.stopPropagation();
+})
+
+$('.top_btn').hide();
+		$(window).scroll(function() {
+			var sh = $(window).scrollTop();
+			
+			if(sh > 300) {
+				$('.top_btn').show(500, 'swing');
+			}else {
+				$('.top_btn').hide(500, 'swing');
+			}
+		});
+		
+		$('.top_btn').click(function() {
+			$('html,body').animate({
+				scrollTop : 0 //到最上面  
+			}, 500);
+		return false;
+		});
+
+const animate = document.querySelector('.award');
+animate.classList.add('fuyofuyo');
+animate.addEventListener('animationend', () => {
+animate.classList.remove('fuyofuyo');  
+setTimeout(function(){
+animate.classList.add('fuyofuyo');
+},2500)
+})
+
+$('.language-pack').hover(function(){
+	$('.switch-icon').attr("src","images/language/icon_language_hover.svg");
+},
+function(){
+	$('.switch-icon').attr("src","images/language/icon_language.svg");
+
+});
